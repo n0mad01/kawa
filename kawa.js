89090e52e66c53d1ea0f56f1afca7b2e7c9a0d43
Adrian Soluch <adrian@soluch.at>
2013-02-26 16:51:50 +0100

    multi level object literals added

--- kawa.js
+++ kawa.js
@@ -6,8 +6,12 @@
 
 ( function( window, undefined ) {
 
-    var element,    // the selected template snippet
-        clone,      // the selected template snippets clone
+    //'use strict';
+
+    var element,        // the selected template snippet
+        clone,          // the selected template snippets clone
+        //object_string = null,  // 
+        exact_matches_only = false,     // only exact mapping will be used e.g. address.street === address.street AND NOT address.street IN user.private.address.street 
         openDelimiter = '{',
         closeDelimiter = '}';
 
@@ -39,9 +43,23 @@
         },
         parseElement : function( node, data ) {
             node = node.firstChild;
+            var n, m, s;
             while( node ) {
                 this.parseElement( node, data );
                 if( node.nodeType === 3 ) {
+                    n = node.data.toString();
+                    m = n.match( openDelimiter+'(.*?)'+closeDelimiter );
+
+                    if( m !== null ) {
+                        s = this.parseObject( data );   // returns an array of all object literal identifiers, e.g.: address.private.zip
+
+                        match = this.matchObjectToElement( s, m[1] );
+
+                        if( match !== null ) {
+                            node.data = node.data.replace( openDelimiter + m[1] + closeDelimiter, match.split('.').reduce( this.index, data ) ); // the magic
+                        }
+                    }
+
                     for( var key in data ) {
                         if( data.hasOwnProperty( key ) ) {
                             if( node.data.indexOf( openDelimiter + key + closeDelimiter ) !== -1 ) {
@@ -53,6 +71,50 @@
                 }
                 node = node.nextSibling;
             }
+        },
+        index : function( data, i ) { 
+            return data[i] 
+        },
+        matchObjectToElement : function( o, e ) {
+
+            ret = null;
+
+            for( var key in o ) {
+                if( o[key] === e ) {
+                    ret = o[key];
+                }
+                else {
+                    if( ! exact_matches_only ) {
+                        if( o[key].indexOf( e ) !== -1 ) {
+                            ret = o[key];
+                        }
+                    }
+                }
+            }
+            return ret;
+        },
+        parseObject : function( data ) {
+
+            var s = new String;
+            var last_object_name = new String;
+            var o = [];
+
+            var recursive = function( data, root ) {
+                for( var key in data ) {
+                    if( typeof data[key] === 'object' ) {
+                        last_object_name = root + key + '.';
+                        recursive( data[key], last_object_name ); // Recursive call
+                    }
+                    else {
+                        s = last_object_name + key;
+                        o.push( s );
+                    }
+                }
+                last_object_name = '';  // next node, void root again
+            };
+            recursive( data, '' );
+
+            return o;
         }
     };
 })( this );
